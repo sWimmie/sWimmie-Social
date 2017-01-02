85f00a4b1a921d37464084871ca57461adcfe590
sWimmie <wim@swimmie.nl>
2017-01-02 21:54:56 +0100

    First commit Firebase set-up

---
+++ Pods/Target Support Files/Pods-sWimmieSocial/Pods-sWimmieSocial-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_sWimmieSocialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_sWimmieSocialVersionString[];
+
---
+++ sWimmieSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  sWimmieSocial
+//
+//  Created by Wim van Deursen on 02-01-17.
+//  Copyright © 2017 sWimmie. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
