fdf2175024200a02cd9d1d5278e9e35ddf691409
sWimmie <wim@swimmie.nl>
2017-01-05 23:23:06 +0100

    Added the UI for the social feed

--- sWimmieSocial/CircleView.swift
+++ sWimmieSocial/CircleView.swift
@@ -1,24 +1,28 @@
 //
-//  fancyView.swift
+//  CircleView.swift
 //  sWimmieSocial
 //
-//  Created by Wim van Deursen on 03-01-17.
+//  Created by Wim van Deursen on 05-01-17.
 //  Copyright © 2017 sWimmie. All rights reserved.
 //
 
 import UIKit
 
-class fancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
         
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
